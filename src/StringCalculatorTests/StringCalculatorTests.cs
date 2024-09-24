16fa4a745b39371cdcb8949b38b1df66c8b80d1e
Joseph Mortleman <joseph@mortleman.com>
2024-09-24 20:29:26 +0100

    Add tests for passing multiple numbers to calculator

--- src/StringCalculatorTests/StringCalculatorTests.cs
+++ src/StringCalculatorTests/StringCalculatorTests.cs
@@ -39,4 +39,16 @@ public class StringCalculatorTests
         var result = calculator.Add(numbers);
         Assert.Equal(expected, result);
     }
+
+    [Theory]
+    [InlineData("1,2,3", 6)]
+    [InlineData("3,2,1,8", 14)]
+    [InlineData("4,4,0,10,0,1", 19)]
+    public void Add_AnyAmountOfNumbers_ReturnsSum(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(numbers);
+        Assert.Equal(expected, result);
+    }
 }
