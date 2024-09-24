13e25c9ea4af3233ecbeb0ddb6cfc67e515bc2df
Joseph Mortleman <joseph@mortleman.com>
2024-09-24 22:13:28 +0100

    Remove Add_TwoNumbers_ReturnsSum test and combine with MultipleNumbers test

--- src/StringCalculatorTests/StringCalculatorTests.cs
+++ src/StringCalculatorTests/StringCalculatorTests.cs
@@ -30,21 +30,10 @@ public class StringCalculatorTests
 
     [Theory]
     [InlineData("1,2", 3)]
-    [InlineData("3,2", 5)]
-    [InlineData("4,4", 8)]
-    public void Add_TwoNumbers_ReturnsSum(string numbers, int expected)
-    {
-        var calculator = new StringCalculator();
-
-        var result = calculator.Add(numbers);
-        Assert.Equal(expected, result);
-    }
-
-    [Theory]
     [InlineData("1,2,3", 6)]
     [InlineData("3,2,1,8", 14)]
     [InlineData("4,4,0,10,0,1", 19)]
-    public void Add_AnyAmountOfNumbers_ReturnsSum(string numbers, int expected)
+    public void Add_MultipleNumbers_ReturnsSum(string numbers, int expected)
     {
         var calculator = new StringCalculator();
 
